0422730c357ff4241c6f21e7882c1c3eb8b4f246
Konstantin-Filipov <kosio1234545@gmail.com>
2023-09-04 12:58:52 +0200

    integrate mongodb in node

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var express = require('express');
+require('dotenv').config();
+
 // Import the mongoose module
 const mongoose = require("mongoose");
+const express = require('express');
+
 const app = express();
-const port = 3000;
+let PORT = process.env.PORT;
+
+mongoose.connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+});  
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
@@ -10,21 +23,6 @@ app.get('/', (req, res) => {        //get requests to the root ("/") will route 
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`); 
+app.listen(PORT, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port ${PORT}`); 
 });
-
-
-// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
-// Included because it removes preparatory warnings for Mongoose 7.
-// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
-mongoose.set("strictQuery", false);
-
-// Define the database URL to connect to.
-const mongoDB = "https://cloud.mongodb.com/v2/644432f16b0e7806d403c050#/metrics/replicaSet/64f2864365cac052c8835381/explorer/music_DB/albums";
-
-// Wait for database to connect, logging an error if there is a problem
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
-}
