da0c1eec2e48cb0a834233af4ea64404093f83a6
Konstantin-Filipov <kosio1234545@gmail.com>
2023-09-06 18:10:01 +0200

    edit index.jx

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const express = require('express');
 const app = express();
 
+
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {console.log("Connected to MongoDB");})
     .catch((err) => {console.error("Error connecting to MongoDB:", err);
@@ -18,7 +19,7 @@ app.use(express.static('front'));
 app.use('/api/albums', albums);
 app.use('/', home);
 
+
+//listen to port- {PORT}
 const PORT = process.env.PORT;
-app.listen(PORT, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${PORT}`); 
-});
+app.listen(PORT, () => {console.log(`Now listening on port ${PORT}`);});
