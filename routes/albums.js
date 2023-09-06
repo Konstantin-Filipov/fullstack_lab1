2a0e96399d7a62720a78f7530a18517e9d309914
Konstantin-Filipov <kosio1234545@gmail.com>
2023-09-06 18:12:41 +0200

    add routes for delete and get

--- routes/albums.js
+++ routes/albums.js
@@ -3,12 +3,32 @@ const router = express.Router();
 const { Album, validate } = require('../models/album')
 const mongoose = require("mongoose");
 
-// Get all the users
+// Get all the albums
 router.get('/', async (req, res) => {
     const albums = await Album
         .find()
         .select({ _id: 1, title: 1, artist: 1 , year: 1});
     res.send(albums);
+});
+
+// Delete an album
+router.delete('/:id', async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) return res.status(400).json({ error: 'Not a valid ID format.' })
+
+    const album = await Album.findByIdAndRemove(req.params.id);
+    if (!album) return res.status(404).json({ error: 'No match for the ID.' });
+
+    res.status(200).json(album);
+});
+
+// Get a user
+router.get('/:id', async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) return res.status(400).json({ error: 'Not a valid ID format.' })
+
+    const album = await Album.findById(req.params.id);
+    if (!album) return res.status(404).json({error: 'No match for the ID.' });
+
+    res.status(200).json(album);
 });
 
 module.exports = router;
