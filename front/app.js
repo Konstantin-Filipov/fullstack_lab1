21e4a7a05e13331416c22fd911f0f65551b84e80
Konstantin-Filipov <kosio1234545@gmail.com>
2023-09-06 18:11:15 +0200

    generate album table, add controllers

---
+++ front/app.js
@@ -0,0 +1,67 @@
+
+window.onload = generateAlbumTable;
+const infoDisplay = document.getElementById('album-creation-error');
+
+async function generateAlbumTable() {
+    let myTable = document.getElementById('album-table');
+    myTable.innerHTML = `
+        <tr><th colspan="6">Albums</th></tr>
+        <tr>
+            <th>Album ID</th>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Year</th>
+            <th>Edit</th>
+            <th>Delete</th>
+
+        </tr>`;
+
+    const data = await getAlbums()
+    data.forEach(element => {
+        let newRow = document.createElement("tr");
+        Object.values(element).forEach((value) => {
+           let cell = document.createElement("td");
+           cell.innerText = String(value);
+           newRow.appendChild(cell);
+        });
+
+        const editAlbum = document.createElement("td")
+        editAlbum.innerHTML = `<button type="button" onclick="generateEditAlbumField(this)">Edit</button>`;
+        newRow.appendChild(editAlbum);
+
+        const deleteAlbum = document.createElement("td")
+        deleteAlbum.innerHTML = `<button type="button" onclick="deleteAlbum(this)">Delete</button>`;
+        newRow.appendChild(deleteAlbum);
+
+        myTable.appendChild(newRow);
+    });
+}
+
+
+async function deleteAlbum(ele) {
+    const albumId = ele.parentElement.parentElement.firstChild.textContent;
+    console.log(albumId);
+    const response = await deleteData(albumId);
+    const data = await response.json()
+
+    infoDisplay.innerHTML = (response.status !== 200) ? data.error : '';
+
+    await generateAlbumTable();
+}
+
+
+//--------------controllers--------------
+async function getAlbums() {
+    return await fetch('api/albums/')
+        .then(response => response.json());
+}
+
+async function deleteData(albumId) {
+    return await fetch(`api/albums/${albumId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    
+}
